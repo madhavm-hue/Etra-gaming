0c3e10014031ee2dd9ccdabe9a7728351fd527eb
Madhavan <madhavan@Madhavans-MacBook-Air.local>
2026-07-24 19:09:17 +0530

    Complete ETRA Gaming website v1

--- src/app/components/Technologies/Technologies.jsx
+++ src/app/components/Technologies/Technologies.jsx
@@ -1,14 +1,64 @@
+import {
+  SiUnrealengine,
+  SiUnity,
+  SiBlender,
+  SiAutodesk,
+} from "react-icons/si";
+
+import {
+  TbCube3dSphere,
+  TbMovie,
+  TbBrush,
+} from "react-icons/tb";
+
+import {
+  GiMagicSwirl,
+} from "react-icons/gi";
+
+import "./technologies.css";
 import "./technologies.css";
 
 const technologies = [
-  "Unreal Engine",
-  "Unity",
-  "Blender",
-  "Autodesk Maya",
-  "Houdini",
-  "Substance 3D",
-  "After Effects",
-  "ZBrush",
+  {
+    number: "01",
+    title: "Unreal Engine",
+    icon: <SiUnrealengine />,
+  },
+  {
+    number: "02",
+    title: "Unity",
+    icon: <SiUnity />,
+  },
+  {
+    number: "03",
+    title: "Blender",
+    icon: <SiBlender />,
+  },
+  {
+    number: "04",
+    title: "Autodesk Maya",
+    icon: <SiAutodesk />,
+  },
+  {
+    number: "05",
+    title: "Houdini",
+    icon: <GiMagicSwirl />,
+  },
+  {
+    number: "06",
+    title: "Substance 3D",
+    icon: <TbCube3dSphere />,
+  },
+{
+  number: "07",
+  title: "After Effects",
+  icon: <TbMovie />,
+},
+{
+  number: "08",
+  title: "ZBrush",
+  icon:  <TbBrush />,
+},
 ];
 
 export default function Technologies() {
@@ -25,15 +75,19 @@ export default function Technologies() {
         </div>
 
         <div className="technologies-grid">
-          {technologies.map((technology, index) => (
-            <div className="technology-card" key={technology}>
-              <span className="technology-number">
-                {String(index + 1).padStart(2, "0")}
-              </span>
+    {technologies.map((technology) => (
+  <div className="technology-card" key={technology.number}>
+    <span className="technology-number">
+      {technology.number}
+    </span>
 
-              <h3>{technology}</h3>
-            </div>
-          ))}
+    <div className="technology-icon">
+      {technology.icon}
+    </div>
+
+    <h3>{technology.title}</h3>
+  </div>
+))}
         </div>
       </div>
     </section>
